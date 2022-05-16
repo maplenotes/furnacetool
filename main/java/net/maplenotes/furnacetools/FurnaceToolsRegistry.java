8ea28491854342edcc8fb9456d28cf249d0f8d79
maplenotes <kaedenomiya@maplenotes.net>
2022-05-16 17:41:13 +0900

    ベーシックツールの登録処理追加

--- main/java/net/maplenotes/furnacetools/FurnaceToolsRegistry.java
+++ main/java/net/maplenotes/furnacetools/FurnaceToolsRegistry.java
@@ -1,12 +1,12 @@
 package net.maplenotes.furnacetools;
 
 import net.maplenotes.furnacetools.materials.Material;
+import net.maplenotes.furnacetools.materials.MaterialFurnace;
 import net.minecraftforge.common.util.EnumHelper;
 
 public class FurnaceToolsRegistry {
 	
-	public static void AddToolMaterial(Material material){
-
+	private static void AddToolMaterial(Material material){
 		EnumHelper.addToolMaterial(
 			material.Name(), 
 			material.HarvestLevel(), 
@@ -15,7 +15,10 @@ public class FurnaceToolsRegistry {
 			material.Damage(), 
 			material.Enchantability()
 		);
+	}
 
+	static {
+		FurnaceToolsRegistry.AddToolMaterial(new MaterialFurnace());
 	}
 
 }
