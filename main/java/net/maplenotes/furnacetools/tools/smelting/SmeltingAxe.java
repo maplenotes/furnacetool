28ee70efaa0dfe612372e998012f5ed5ba864a39
maplenotes <kaedenomiya@maplenotes.net>
2022-05-18 14:02:09 +0900

    精錬系ツールのベース作成

---
+++ main/java/net/maplenotes/furnacetools/tools/smelting/SmeltingAxe.java
@@ -0,0 +1,12 @@
+package net.maplenotes.furnacetools.tools.smelting;
+
+import net.maplenotes.furnacetools.materials.Material;
+import net.maplenotes.furnacetools.tools.FurnaceAxe;
+
+public class SmeltingAxe extends FurnaceAxe {
+
+	protected SmeltingAxe(Material material) {
+		super(material);
+	}
+	
+}
---
+++ main/java/net/maplenotes/furnacetools/tools/smelting/SmeltingPickaxe.java
@@ -0,0 +1,13 @@
+package net.maplenotes.furnacetools.tools.smelting;
+
+import net.maplenotes.furnacetools.materials.Material;
+import net.maplenotes.furnacetools.tools.FurnacePickaxe;
+
+public class SmeltingPickAxe extends FurnacePickaxe {
+
+	protected SmeltingPickAxe(Material material) {
+		super(material);
+	}
+	
+	
+}
