0a06d4b861f48f433aa449eb4a49c5205607329b
maplenotes <kaedenomiya@maplenotes.net>
2022-06-09 01:45:03 +0900

    精錬回収について、発動条件の制約を課した

--- main/java/net/maplenotes/furnacetools/event/SmeltingBehavior.java
+++ main/java/net/maplenotes/furnacetools/event/SmeltingBehavior.java
@@ -1,13 +1,23 @@
 package net.maplenotes.furnacetools.event;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTool;
 import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
 
 public class SmeltingBehavior {
     
     public static void onHarvest(HarvestDropsEvent event) {
+
+        EntityPlayer player = event.getHarvester();
+
+        // スニーキング状態での回収のみ、精錬回収が発動できる
+        if(!player.isSneaking()) {
+            return;
+        }
+
         IBlockState state = event.getState();
 
         // 回収対象ブロックが精錬レシピに存在するか確認する
@@ -18,9 +28,26 @@ public class SmeltingBehavior {
         if(smeltingResult == ItemStack.EMPTY) {
             return;
         }
+        
+        ItemStack heldItem = player.getHeldItem(player.getActiveHand());
+
+        if(heldItem == ItemStack.EMPTY) {
+            return;
+        }
+        if(!(heldItem.getItem() instanceof ItemTool)) {
+            return;
+        }
+
+        ItemTool tool = (ItemTool)heldItem.getItem();
+
+        // ツールが得意としていないブロックを回収しようとしていた場合、精錬回収は発動しない
+        if(tool.getDestroySpeed(heldItem, state) <= 1.0F) {
+            return;
+        }
 
         event.getDrops().clear();
         event.getDrops().add(new ItemStack(smeltingResult.getItem(), 1));
+        heldItem.damageItem(1, player);
     }
 
 }
