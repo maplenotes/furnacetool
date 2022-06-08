9404e979db850a7082ba513e4032690e047b7fba
maplenotes <kaedenomiya@maplenotes.net>
2022-06-09 02:52:27 +0900

    焼き石がそのまま回収できるよう修正

--- main/java/net/maplenotes/furnacetools/event/SmeltingBehavior.java
+++ main/java/net/maplenotes/furnacetools/event/SmeltingBehavior.java
@@ -1,7 +1,11 @@
 package net.maplenotes.furnacetools.event;
 
+import net.minecraft.block.BlockStone;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTool;
 import net.minecraft.item.crafting.FurnaceRecipes;
@@ -12,25 +16,35 @@ public class SmeltingBehavior {
     public static void onHarvest(HarvestDropsEvent event) {
 
         EntityPlayer player = event.getHarvester();
-
         // スニーキング状態での回収のみ、精錬回収が発動できる
         if(!player.isSneaking()) {
             return;
         }
 
+        // シルクタッチがあるときは、精錬回収できない
+        if(event.isSilkTouching()) {
+            return;
+        }
+
+        ItemStack heldItem = player.getHeldItem(player.getActiveHand());
         IBlockState state = event.getState();
+        ItemStack targetBlock = new ItemStack(state.getBlock(), 1);
+
+        // 特殊実装:例外判定 焼き石を回収した時、(丸石ではなく)焼き石をドロップする
+        if((state.getBlock() instanceof BlockStone) && state.getBlock().getMetaFromState(state) == 0 && (heldItem.getItem() instanceof ItemPickaxe)) {
+            event.getDrops().removeIf(x -> x.getItem() == Item.getItemFromBlock(Blocks.COBBLESTONE));
+            event.getDrops().add(new ItemStack(Blocks.STONE, 1, 0));
+            return;
+        }
 
         // 回収対象ブロックが精錬レシピに存在するか確認する
         // 精錬レシピにない場合、 ItemStack.EMPTY が返る
-        ItemStack targetBlock = new ItemStack(state.getBlock(), 1);
         ItemStack smeltingResult = FurnaceRecipes.instance().getSmeltingResult(targetBlock);
 
         if(smeltingResult == ItemStack.EMPTY) {
             return;
         }
         
-        ItemStack heldItem = player.getHeldItem(player.getActiveHand());
-
         if(heldItem == ItemStack.EMPTY) {
             return;
         }
@@ -45,8 +59,8 @@ public class SmeltingBehavior {
             return;
         }
 
-        event.getDrops().clear();
-        event.getDrops().add(new ItemStack(smeltingResult.getItem(), 1));
+        event.getDrops().removeIf(x -> x.getItem() == Item.getItemFromBlock(state.getBlock()) && x.getMetadata() == state.getBlock().getMetaFromState(state));
+        event.getDrops().add(targetBlock);
         heldItem.damageItem(1, player);
     }
 
