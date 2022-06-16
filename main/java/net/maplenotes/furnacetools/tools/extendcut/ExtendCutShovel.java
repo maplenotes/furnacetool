33d5f02cd32c85769805f4b2670ef3395ba3305c
maplenotes <kaedenomiya@maplenotes.net>
2022-06-17 02:30:27 +0900

    拡張炉シャベルのブロック破壊時イベント追加

--- main/java/net/maplenotes/furnacetools/tools/extendcut/ExtendCutShovel.java
+++ main/java/net/maplenotes/furnacetools/tools/extendcut/ExtendCutShovel.java
@@ -2,11 +2,21 @@ package net.maplenotes.furnacetools.tools.extendcut;
 
 import net.maplenotes.furnacetools.materials.Material;
 import net.maplenotes.furnacetools.tools.FurnaceShovel;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class ExtendCutShovel extends FurnaceShovel {
 
     public ExtendCutShovel(Material material) {
         super(material);
+    }
+
+    @Override
+    public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState state, BlockPos orig, EntityLivingBase entityLiving) {
+        return HookEvent.onBlockDestroyed(stack, worldIn, state, orig, entityLiving);
     }
 	
 }
