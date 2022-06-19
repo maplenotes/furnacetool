9b426e84701671fcd5765482da4dbe1fb0d022c6
maplenotes <kaedenomiya@maplenotes.net>
2022-06-19 20:26:51 +0900

    拡張炉クワのイベント処理メソッドのベースを作成

--- main/java/net/maplenotes/furnacetools/tools/extendcut/ExtendCutHoe.java
+++ main/java/net/maplenotes/furnacetools/tools/extendcut/ExtendCutHoe.java
@@ -2,6 +2,15 @@ package net.maplenotes.furnacetools.tools.extendcut;
 
 import net.maplenotes.furnacetools.materials.Material;
 import net.maplenotes.furnacetools.tools.FurnaceHoe;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class ExtendCutHoe extends FurnaceHoe {
 
@@ -9,4 +18,14 @@ public class ExtendCutHoe extends FurnaceHoe {
 		super(material);
 	}
 	
+	@Override
+    public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState state, BlockPos orig, EntityLivingBase entityLiving) {
+        return true;
+    }
+
+	@Override
+	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		return EnumActionResult.SUCCESS;
+	}
+
 }
